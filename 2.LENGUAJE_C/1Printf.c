30c0b4804b5a7d6bec7358c9eec73b5198a8d1bf
jennyGuadalupeAH <jennyangeleshernandez310@gmail.com>
2023-06-08 02:55:40 -0600

    Codigos en C Metodos Numericos

---
+++ 2.LENGUAJE_C/1Printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("Este es un texto \n");
+	printf("que fue impreso desde C");
+	printf("\n");
+	printf("Soy todo un programador");
+	printf("\n");
+	system("Pause");
+	return 0;
+}
---
+++ 2.LENGUAJE_C/2BienvenidaJ.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("Bienvenido\n");
+	printf("Es un gusto compilar para \n");
+	printf("Jenni \n");
+	system("Pause");
+	return 0;
+}
